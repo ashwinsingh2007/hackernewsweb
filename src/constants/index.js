2172722a744c0586b7add154e948fce2d95e4322
Ashwin Singh <legend.ashwini07@gmail.com>
2019-07-27 20:03:45 +0530

    finishing react

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,4 @@
-export const BASE_API_URL = 'https://hacker-news.firebaseio.com/v0/'
-export const YCOMB_DOMAIN = 'https://www.ycombinator.com'
-export const NEWS_YCOMB_DOMAIN = 'https://news.ycombinator.com/'
-export const NEWS_DOMAIN = '/'
-
-export const MAX_THREAD_NUMBER = 30
-// const baseUrl = 'http://localhost:9000';
-const baseUrl = 'http://13.232.63.148:9000';
+const baseUrl = 'http://localhost:9000';
 export const GET_NEWS_URL = `${baseUrl}/api/news`
 export const GET_COMMMENTS_URL = `${baseUrl}/api/comments`
 export const GET_COMMMENTS_EDIT_URL = `${baseUrl}/api/comments/edit`
